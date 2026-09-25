b2d1e0e79a4f8346bfd35dc3c78a68fc25bd4b64
Jack Anderson <jaandersonck@gmail.com>
2026-09-25 11:25:26 -0400

    implement overloads and seed methods

--- exercises/ex06-overloading/src/main/java/MyHashing.java
+++ exercises/ex06-overloading/src/main/java/MyHashing.java
@@ -19,7 +19,7 @@ public class MyHashing {
 
   /** Creates a MyHashing whose seed starts at 0. */
   public MyHashing() {
-    // TODO: this constructor takes no arguments; leave the seed at its default.
+    this.seed = 0;
   }
 
   /**
@@ -28,7 +28,7 @@ public class MyHashing {
    * @param seed the initial seed value
    */
   public MyHashing(int seed) {
-    // TODO: store the parameter in this object's seed field.
+    this.seed = seed;
   }
 
   /**
@@ -38,8 +38,9 @@ public class MyHashing {
    * @return the seed value from before this call
    */
   public int hash(int value) {
-    // TODO
-    return 0;
+    int oldSeed = seed; // no such thing as aliasing for primitives
+    seed = value;
+    return oldSeed;
   }
 
   /**
@@ -51,8 +52,10 @@ public class MyHashing {
    * @return (previous seed + value) % MODULO
    */
   public int hash(char value) {
-    // TODO
-    return 0;
+    int oldSeed = seed;
+    seed = value;
+
+    return (oldSeed + value) % MODULO;
   }
 
   /**
@@ -64,7 +67,10 @@ public class MyHashing {
    * @return the sum of the characters' numeric codes
    */
   public static int hash(String value) {
-    // TODO: String.toCharArray() may help.
-    return 0;
+    int s = 0;
+    for (char c: value.toCharArray()) {
+      s += c;
+    }
+    return s;
   }
 }
